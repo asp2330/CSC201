19080b85160c252d51ceb8cd0ce5185628b52690
asp2330 <asp2330@email.vccs.edu>
2017-02-02 01:27:11 -0500

    Exercise 7.10

---
+++ src/test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+/**
+ * Created by Clue on 2/2/2017.
+ */
+public class test
+{
+    public static void main (String args [])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+
+
+
+    }
+}
